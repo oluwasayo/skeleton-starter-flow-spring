ce2cb0c66328d7218a9bd6482c5d1401464cacd3
Sayo Oladeji <sayo@vaadin.com>
2019-03-13 15:26:13 +0200

    Simplify IT

--- src/test/java/com/vaadin/starter/skeleton/spring/MainViewIT.java
+++ src/test/java/com/vaadin/starter/skeleton/spring/MainViewIT.java
@@ -1,18 +1,19 @@
 package com.vaadin.starter.skeleton.spring;
 
 import com.vaadin.testbench.addons.junit5.extensions.container.SpringBootConf;
-import org.junit.jupiter.api.Assertions;
 import com.vaadin.testbench.addons.junit5.extensions.unittest.VaadinWebUnitTest;
 
-@SpringBootConf(source = Application.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @VaadinWebUnitTest
+@SpringBootConf(source = Application.class)
 public class MainViewIT {
 
   @VaadinWebUnitTest
   void testFormSubmission(MainViewPageObject po) {
     po.loadPage();
     po.button().click();
-    Assertions.assertEquals("Button was clicked" ,
-                            po.notification().getText());
+
+    assertEquals("Button was clicked", po.notification().getText());
   }
 }
